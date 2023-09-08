ad6567c8b7e4ab52ed8352058f09e0fdbf22c45c
Matthias <matthias-endler@gmx.net>
2023-09-08 12:20:34 +0200

    Cleanup deps

--- appleconnector/__init__.py
+++ appleconnector/__init__.py
@@ -5,3 +5,5 @@ The API is not documented and may change at any time. Use at your own risk.
 
 from .connector import AppleConnector
 from .connector import Mode, SeriesMode, Metric, Dimension
+
+__all__ = ["AppleConnector", "Mode", "SeriesMode", "Metric", "Dimension"]
